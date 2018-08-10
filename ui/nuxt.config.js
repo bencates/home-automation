edd08496507ce5fa348d2aa9f5c893f9d6b0ddf5
Ben Cates <ben@DESKTOP-KRFI1K6.localdomain>
2018-08-09 22:29:49 -0600

    merge nuxt and phoenix config files

---
+++ ui/nuxt.config.js
@@ -0,0 +1,68 @@
+module.exports = {
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'home',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Home automation control app'
+      }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
+  },
+
+  /*
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+
+  /*
+   ** Nuxt root directory
+   */
+  srcDir: 'assets',
+
+  /*
+   ** Generate into Phoenix static directory
+   */
+  generate: {
+    dir: 'priv/static'
+  }
+}
